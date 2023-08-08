726af0bed90ad95eb1e79aef91ecb7ef1867622a
Kiril Kovachev <kiril.kovachev@sap.com>
2023-08-08 17:28:23 +0300

    Added mariunder board base config

---
+++ components/retro-go/targets/mariunder-one/config.h
@@ -0,0 +1,85 @@
+// REF: https://wiki.odroid.com/odroid_go/odroid_go
+
+// Target definition
+#define RG_TARGET_NAME             "MARIUNDER-ONE"
+
+// Storage
+#define RG_STORAGE_DRIVER           1                   // 0 = Host, 1 = SDSPI, 2 = SDMMC, 3 = USB, 4 = Flash
+#define RG_STORAGE_HOST             SDMMC_HOST_SLOT_1   // Used by driver 1 and 2
+#define RG_STORAGE_SPEED            SDMMC_FREQ_DEFAULT  // Used by driver 1 and 2
+#define RG_STORAGE_ROOT             "/sd"               // Storage mount point
+
+// Audio
+#define RG_AUDIO_USE_INT_DAC        0   // 0 = Disable, 1 = GPIO25, 2 = GPIO26, 3 = Both
+#define RG_AUDIO_USE_EXT_DAC        1   // 0 = Disable, 1 = Enable
+
+// Video
+#define RG_SCREEN_DRIVER            0   // 0 = ILI9341
+#define RG_SCREEN_HOST              SPI2_HOST
+#define RG_SCREEN_SPEED             SPI_MASTER_FREQ_80M
+#define RG_SCREEN_TYPE              4   // 4 = ESPLAY-ST7789V2
+#define RG_SCREEN_WIDTH             320
+#define RG_SCREEN_HEIGHT            240
+#define RG_SCREEN_ROTATE            0
+#define RG_SCREEN_MARGIN_TOP        0
+#define RG_SCREEN_MARGIN_BOTTOM     0
+#define RG_SCREEN_MARGIN_LEFT       0
+#define RG_SCREEN_MARGIN_RIGHT      0
+
+// Input
+#define RG_GAMEPAD_DRIVER           1   // 1 = ODROID-GO, 2 = Serial, 3 = I2C
+#define RG_GAMEPAD_HAS_MENU_BTN     1
+#define RG_GAMEPAD_HAS_OPTION_BTN   1
+// Note: Depending on the driver, the button map can be a bitmask, an index, or a GPIO.
+// Refer to rg_input.h to see all available RG_KEY_*
+#define RG_GAMEPAD_MAP {\
+    {RG_KEY_SELECT, RG_GPIO_GAMEPAD_SELECT},\
+    {RG_KEY_START,  RG_GPIO_GAMEPAD_START},\
+    {RG_KEY_MENU,   RG_GPIO_GAMEPAD_MENU},\
+    {RG_KEY_OPTION, RG_GPIO_GAMEPAD_OPTION},\
+    {RG_KEY_A,      RG_GPIO_GAMEPAD_A},\
+    {RG_KEY_B,      RG_GPIO_GAMEPAD_B},\
+}
+
+// Battery
+#define RG_BATTERY_ADC_CHANNEL      ADC1_CHANNEL_3
+#define RG_BATTERY_CALC_PERCENT(raw) (((raw) * 2.f - 3500.f) / (4200.f - 3500.f) * 100.f)
+#define RG_BATTERY_CALC_VOLTAGE(raw) ((raw) * 2.f * 0.001f)
+
+// Status LED
+#define RG_GPIO_LED                 GPIO_NUM_40
+
+// I2C BUS
+//#define RG_GPIO_I2C_SDA             GPIO_NUM_10
+//#define RG_GPIO_I2C_SCL             GPIO_NUM_11
+
+// Built-in gamepad
+#define RG_GPIO_GAMEPAD_X           ADC1_CHANNEL_5
+#define RG_GPIO_GAMEPAD_Y           ADC1_CHANNEL_4
+#define RG_GPIO_GAMEPAD_SELECT      GPIO_NUM_17
+#define RG_GPIO_GAMEPAD_START       GPIO_NUM_35
+#define RG_GPIO_GAMEPAD_A           GPIO_NUM_37
+#define RG_GPIO_GAMEPAD_B           GPIO_NUM_36
+#define RG_GPIO_GAMEPAD_MENU        GPIO_NUM_15
+#define RG_GPIO_GAMEPAD_OPTION      GPIO_NUM_16
+
+// SPI Display
+#define RG_GPIO_LCD_MISO            GPIO_NUM_13
+#define RG_GPIO_LCD_MOSI            GPIO_NUM_11
+#define RG_GPIO_LCD_CLK             GPIO_NUM_12
+#define RG_GPIO_LCD_CS              GPIO_NUM_10
+#define RG_GPIO_LCD_DC              GPIO_NUM_21
+#define RG_GPIO_LCD_BCKL            GPIO_NUM_14
+#define RG_GPIO_LCD_RST             GPIO_NUM_NC
+
+// SPI SD Card
+#define RG_GPIO_SDSPI_MISO          GPIO_NUM_13
+#define RG_GPIO_SDSPI_MOSI          GPIO_NUM_11
+#define RG_GPIO_SDSPI_CLK           GPIO_NUM_12
+#define RG_GPIO_SDSPI_CS            GPIO_NUM_9
+
+// External I2S DAC
+#define RG_GPIO_SND_I2S_BCK         GPIO_NUM_42
+#define RG_GPIO_SND_I2S_WS          GPIO_NUM_41
+#define RG_GPIO_SND_I2S_DATA        GPIO_NUM_47
+#define RG_GPIO_SND_AMP_ENABLE      GPIO_NUM_48
