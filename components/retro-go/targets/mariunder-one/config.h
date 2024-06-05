9a45e6d6caae6262631aa1f0d328763fdea44287
Kiril Kovachev <kiril.kovachev@sap.com>
2024-06-05 17:23:42 +0300

    Updated code to support Mariunder-one

--- components/retro-go/targets/mariunder-one/config.h
+++ components/retro-go/targets/mariunder-one/config.h
@@ -1,7 +1,6 @@
-// REF: https://wiki.odroid.com/odroid_go/odroid_go
-
 // Target definition
-#define RG_TARGET_NAME              MARIUNDER-ONE
+
+#define RG_TARGET_NAME              "MARIUNDER-ONE"
 
 // Storage
 #define RG_STORAGE_DRIVER           1                   // 0 = Host, 1 = SDSPI, 2 = SDMMC, 3 = USB, 4 = Flash
@@ -25,22 +24,22 @@
 #define RG_SCREEN_MARGIN_BOTTOM     0
 #define RG_SCREEN_MARGIN_LEFT       0
 #define RG_SCREEN_MARGIN_RIGHT      0
-#define RG_SCREEN_INIT()
-	ILI9341_CMD(0xC5, {0x1A}); //VCOM
-	ILI9341_CMD(0x36, {0x20}); //Display Rotation
-	ILI9341_CMD(0xB2, {0x05, 0x05, 0x00, 0x33, 0x33});  //Porch Setting
-	ILI9341_CMD(0xB7, {0x05});  //Gate Control //12.2v   -10.43v
-	ILI9341_CMD(0xBB, {0x3F});  //VCOM
-	ILI9341_CMD(0xC0, {0x2c});  //Power control
-	ILI9341_CMD(0xC2, {0x01});  //VDV and VRH Command Enable
-	ILI9341_CMD(0xC3, {0x0F});  //VRH Set 4.3+( vcom+vcom offset+vdv)
-	ILI9341_CMD(0xC4, {0xBE});  //VDV Set 0v
-	ILI9341_CMD(0xC6, {0X01});  //Frame Rate Control in Normal Mode 111Hz
-	ILI9341_CMD(0xD0, {0xA4,0xA1});  //Power Control 1
-	ILI9341_CMD(0xE8, {0x03});   //Power Control 1
-	ILI9341_CMD(0xE9, {0x09,0x09,0x08});  //Equalize time control
-	ILI9341_CMD(0xE0, {0xD0,0x05,0x09,0x09,0x08,0x14,0x28,0x33,0x3F,0x07,0x13,0x14,0x28,0x30});   //Set Gamma
-	ILI9341_CMD(0xE1, {0xD0, 0x05, 0x09, 0x09, 0x08, 0x03, 0x24, 0x32, 0x32, 0x3B, 0x14, 0x13, 0x28, 0x2F, 0x1F});   //Set Gamma
+#define RG_SCREEN_INIT() \
+	ILI9341_CMD(0xC5, 0x1A); /* VCOM */ \
+	ILI9341_CMD(0x36, 0x20); /* Display Rotation  */ \
+	ILI9341_CMD(0xB2, 0x05, 0x05, 0x00, 0x33, 0x33);  /* Porch Setting */ \
+	ILI9341_CMD(0xB7, 0x05);  /* Gate Control //12.2v   -10.43v */ \
+	ILI9341_CMD(0xBB, 0x3F);  /* VCOM */ \
+	ILI9341_CMD(0xC0, 0x2c);  /* Power control */ \
+	ILI9341_CMD(0xC2, 0x01);  /* VDV and VRH Command Enable */ \
+	ILI9341_CMD(0xC3, 0x0F);  /* VRH Set 4.3+( vcom+vcom offset+vdv) */ \
+	ILI9341_CMD(0xC4, 0xBE);  /* VDV Set 0v */ \
+	ILI9341_CMD(0xC6, 0X01);  /* Frame Rate Control in Normal Mode 111Hz */ \
+	ILI9341_CMD(0xD0, 0xA4,0xA1);  /* Power Control 1 */ \
+	ILI9341_CMD(0xE8, 0x03);   /* Power Control 1 */ \
+	ILI9341_CMD(0xE9, 0x09,0x09,0x08);  /* Equalize time control */ \
+	ILI9341_CMD(0xE0, 0xD0,0x05,0x09,0x09,0x08,0x14,0x28,0x33,0x3F,0x07,0x13,0x14,0x28,0x30);   /* Set Gamma */ \
+	ILI9341_CMD(0xE1, 0xD0, 0x05, 0x09, 0x09, 0x08, 0x03, 0x24, 0x32, 0x32, 0x3B, 0x14, 0x13, 0x28, 0x2F, 0x1F);   /* Set Gamma */
 
 // Input
 #define RG_GAMEPAD_DRIVER           1   // 1 = ODROID-GO, 2 = Serial, 3 = I2C
